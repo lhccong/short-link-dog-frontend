4d5f2ba5d8d69712c6170866ed02263d80405a05
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-01-31 09:30:27 +0800

    fix: 移除多余弹窗

--- src/access.ts
+++ src/access.ts
@@ -3,7 +3,6 @@
  * */
 export default function access(initialState: { currentUser?: API.LoginUserVO } | undefined) {
   const {currentUser} = initialState ?? {};
-  alert(currentUser);
   return {
     canUser: !(currentUser===undefined||currentUser===null),
     canAdmin: currentUser && currentUser.userRole === 'admin',
