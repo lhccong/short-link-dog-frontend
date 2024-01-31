a3f133cfb8595b06c4ac8833c5ad3baf1fac69ad
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-01-31 09:58:51 +0800

    fix: 修复用户登录校验异常

--- src/requestConfig.ts
+++ src/requestConfig.ts
@@ -1,7 +1,7 @@
 ﻿import { BACKEND_HOST_LOCAL, BACKEND_HOST_PROD } from '@/constants';
 import type { RequestOptions } from '@@/plugin-request/request';
 import type { RequestConfig } from '@umijs/max';
-import {message} from "antd";
+import { message } from 'antd';
 
 // 与后端约定的响应数据格式
 interface ResponseStructure {
@@ -55,7 +55,7 @@ export const requestConfig: RequestConfig = {
       const code: number = data.code;
       // 未登录，且不为获取用户登录信息接口
       if (
-        code === 40100 &&
+        (code === 40100 || code === 50001) &&
         !requestPath.includes('user/get/login') &&
         !location.pathname.includes('/user/login')
       ) {
