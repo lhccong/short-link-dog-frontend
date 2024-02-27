6225afff7d871341d5bf83bbc95d792c0a5249d5
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-02-27 10:32:58 +0800

    fix: 修改api的生成地址

--- config/config.ts
+++ config/config.ts
@@ -129,7 +129,7 @@ export default defineConfig({
   openAPI: [
     {
       requestLibPath: "import { request } from '@umijs/max'",
-      schemaPath: 'http://localhost:8101/api/v2/api-docs',
+      schemaPath: 'http://localhost:8204/api/v2/api-docs',
       projectName: 'backend',
     },
   ],
